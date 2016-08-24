54ba95087c6880bd1255ef8b3a97d06b004eee60
Erick Lavoie <github@ericklavoie.com>
2016-08-24 14:27:37 +0200

    Initial commit

---
+++ runner.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "common.h"
+//#include "./common/ostrich_twister_rand.c"
+//#include "./kernel.c"
+//#include "./verify.c"
+
+int main(int argc, char** argv) {
+    int input = 0;
+    double elapsedTime = 0.0;
+    int output = 0;
+
+    // Create or load input data
+    
+    // Run kernel and measure time for core computation
+
+    // Verify output
+    
+
+    // Display output
+    printf("{ \"input\": %d, \"time\": %f, \"output\": %d}", input, elapsedTime, output);
+    return 0;    
+}
